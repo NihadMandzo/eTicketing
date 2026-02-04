41bc5434cd6ec27fa18c1344621ee58b8999b10d
NihadMandzo <nihad.mandzo@edu.fit.ba>
2026-02-04 20:33:47 +0100

    feat: Implement layout structure with top and bottom navigation bars

---
+++ frontend/web/src/app/components/top-navbar/top-navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-top-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './top-navbar.component.html',
+  styleUrl: './top-navbar.component.css'
+})
+export class TopNavbarComponent {
+  
+}
